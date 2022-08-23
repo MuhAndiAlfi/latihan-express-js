47ebecadac358029b7b4e97c508016855aae1578
Muhammad Andi Aflainsyah <akameandi42@gmail.com>
2022-08-23 09:32:43 +0700

    configjs

--- config/config.js
+++ config/config.js
@@ -20,12 +20,19 @@ module.exports={
         dialect: "postgres"
     },
     production: {
-        host: DEV_DB_HOST,
-        database: DEV_DB,
-        username: DEV_DB_USERNAME,
-        password: DEV_DB_PASSWORD,
-        port: DEV_DB_PORT,
-        dialect: "postgres"
+        host: "ec2-34-203-182-65.compute-1.amazonaws.com",
+        database: "d5dftbegg779gn",
+        username: "nacyrghvefqtst",
+        password: "9fa7b4c6c5b0410f962df7b40395fe55a3b9bf027dab5ff513efad76dc14403b",
+        port: "5432",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        url: "postgres://nacyrghvefqtst:9fa7b4c6c5b0410f962df7b40395fe55a3b9bf027dab5ff513efad76dc14403b@ec2-34-203-182-65.compute-1.amazonaws.com:5432/d5dftbegg779gn",
+        herokucli: "heroku pg:psql postgresql-defined-65340 --app latihan-backend-express"
     },
 
 }
